ceef28cacf405e0b1ccfa16f4310d97e9def4d06
Nicola Ward <105745948+NicolaWard@users.noreply.github.com>
2022-05-29 16:34:32 +0100

    Update Guess The Number.py

--- Guess The Number.py
+++ Guess The Number.py
@@ -1,3 +1,8 @@
+#Guess The Number
+# A game where the user can guess a random number that the computer
+# generates and also where the computer can guess a number that
+# the user has thought of.
+
 import random
 
 def guess(x):
@@ -12,5 +17,20 @@ def guess(x):
 
     print (f'Yay, congrats. You have guessed the number {random_number} correctly!')
 
+def computer_guess (x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint (low, high)
+        else:
+            guess = low #Could also be high
+        feedback = input (f'Is {guess} too high? (H) Too low (L) or Correct (C)').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print (f'Yay! The computer guessed the number {guess} correctly!')
 
-guess (10)
+computer_guess (10)
